9a24b8d12eebd552de16ae732de5055b502dbe40
Tiago Gonçalves <tiaguin180@gmail.com>
2021-12-22 12:14:06 -0300

    feat: Using routers

---
+++ src/routes/index.tsx
@@ -0,0 +1,20 @@
+import { BrowserRouter, Switch } from 'react-router-dom';
+
+import { CustomRouter } from './CustomRouter';
+
+import {
+  Login,
+  Register
+} from '../pages';
+
+export function AppRoutes() {
+  return (
+    <BrowserRouter>
+      <Switch>
+        {/* <CustomRouter component={Login} exact path="/" /> */}
+        <CustomRouter component={Login} path="/login" />
+        <CustomRouter component={Register} path="/register" />
+      </Switch>
+    </BrowserRouter>
+  )
+}
