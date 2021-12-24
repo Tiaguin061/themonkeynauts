54b8f1f56ebc3b4d2294d899fbc16e1bc0d272c5
Tiago Gonçalves <tiaguin180@gmail.com>
2021-12-23 21:53:16 -0300

    feat: Added styled to button

--- src/components/HTML/Button/styles.ts
+++ src/components/HTML/Button/styles.ts
@@ -1,5 +1,20 @@
 import styled from 'styled-components';
 
-export const Container = styled.button`
+import button_background_1 from '@/assets/svg/button_background_1.svg';
 
+export const Container = styled.button`
+  background: url(${button_background_1}) no-repeat center;
+  background-size: contain;
+
+  padding: 1.6rem;
+  width: 100%;
+  
+  display: flex;
+  align-items: center;
+  justify-content: center;
+
+  font-weight: bold;
+  line-height: 1.5rem;
+  letter-spacing: 0.035em;
+  text-align: center;
 `;
