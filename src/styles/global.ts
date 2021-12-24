db6e45a10278d3cb61c94e1fc0837b12e0906f2e
Tiago Gonçalves <tiaguin180@gmail.com>
2021-12-23 21:54:39 -0300

    feat: Define global fonts-size

--- src/styles/global.ts
+++ src/styles/global.ts
@@ -1,3 +1,4 @@
+import { COLORS, FONTS } from '@/theme';
 import { createGlobalStyle } from 'styled-components';
 
 export const GlobalStyle = createGlobalStyle`
@@ -5,6 +6,7 @@ export const GlobalStyle = createGlobalStyle`
     margin: 0;
     padding: 0;
     box-sizing: border-box;
+    font-family: ${FONTS.orbitron}, sans-serif;
   }
 
   button {
@@ -23,5 +25,61 @@ export const GlobalStyle = createGlobalStyle`
 
   :root {
     font-size: 62.5%;
+  }
+
+  input {
+    border: none;
+    outline: 0;
+  }
+
+  body, html, #root {
+    min-height: 100vh;
+    width: 100%;
+  }
+
+  h1, h2, h3, h4, h5, h6, a, p, span, label, button {
+    color: ${COLORS.fonts.primary};
+  }
+
+  @media(min-width: 280px) and (max-width: 1399px) {
+    h1 {
+      font-size: 2.4rem;
+      font-weight: bold;
+    }
+
+    h2 {
+      font-size: 1.2rem;
+      font-weight: bold;
+    }
+
+    h3 {
+      font-size: 1.1rem;
+      font-weight: bold;
+    }
+
+    p, span, a, label {
+      font-size: 1.2rem;
+    }
+  }
+  
+  @media(min-width: 1400px) {
+    h1 {
+      font-size: 2.4rem;
+      font-weight: bold;
+    }
+
+    h2 {
+      font-size: 1.8rem;
+      font-weight: bold;
+    }
+
+    h3 {
+      font-size: 1.4rem;
+      font-weight: bold;
+    }
+
+    p, span, a, label {
+      font-size: 1.4rem;
+    }
   }
 `; 
