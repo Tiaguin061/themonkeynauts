c605a188b242fd03fdb3f4432bc4a312f20cb831
Tiago Gonçalves <tiaguin180@gmail.com>
2021-12-23 21:54:06 -0300

    feat: Added auth provider

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,12 @@
+import { AuthProvider } from './contexts';
 import { AppRoutes } from './routes';
 import { GlobalStyle } from './styles/global';
 
 export function App() {
   return (
-    <>
+    <AuthProvider>
       <AppRoutes />
       <GlobalStyle />
-    </>
+    </AuthProvider>
   );
 }
