5ad58f4efc7e127f75dfe28a63ad9148e50bf0f9
pprograminha <marcosproenca144@gmail.com>
2022-05-28 10:58:05 -0300

    feat: completed application folder structure

--- src/shared/infra/helpers/get-balance.ts
+++ src/shared/infra/helpers/get-balance.ts
@@ -1,15 +1,16 @@
 import Web3 from 'web3';
 
-const web3 = new Web3('https://mainnet.infura.io/v3/ea9cb2d6e94444e79abc9cfb9621ced3')
+const web3 = new Web3(
+  'https://mainnet.infura.io/v3/ea9cb2d6e94444e79abc9cfb9621ced3',
+);
 
-const contractAddress = '0x4Fabb145d64652a948d72533023f6E7A623C7C53'
+const contractAddress = '0x4Fabb145d64652a948d72533023f6E7A623C7C53';
 
 const getBalance = async () => {
-    const balance = await web3.eth.getBalance(contractAddress)
-    const balanceFormatted = web3.utils.fromWei(balance, 'ether')
+  const balance = await web3.eth.getBalance(contractAddress);
+  const balanceFormatted = web3.utils.fromWei(balance, 'ether');
 
-    console.log(balance, balanceFormatted)
+  console.log(balance, balanceFormatted);
+};
 
-}
-
-getBalance()
+getBalance();
